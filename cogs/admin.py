9156d52c13bdfba0c1317bb02c461b75bcb09951
Aleytia <scarletdevil25@gmail.com>
2023-02-15 11:30:41 -0500

    Translate admin.py to English

--- cogs/admin.py
+++ cogs/admin.py
@@ -16,51 +16,51 @@ from yuanshen import automation
 class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
-        self.presence_string: list[str] = ["原神"]
+        self.presence_string: list[str] = ["genshin"]
         self.change_presence.start()
 
-    # 同步 Slash commands 到全域或是當前伺服器
-    @app_commands.command(name="sync", description="同步Slash commands到全域或是當前伺服器")
-    @app_commands.rename(area="範圍")
-    @app_commands.choices(area=[Choice(name="當前伺服器", value=0), Choice(name="全域伺服器", value=1)])
+    # sync Slash commands to current server
+    @app_commands.command(name="sync", description="sync Slash commands to current server")
+    @app_commands.rename(area="area")
+    @app_commands.choices(area=[Choice(name="current server", value=0), Choice(name="global server", value=1)])
     @SlashCommandLogger
     async def slash_sync(self, interaction: discord.Interaction, area: int = 0):
         await interaction.response.defer()
-        if area == 0 and interaction.guild:  # 複製全域指令，同步到當前伺服器，不需等待
+        if area == 0 and interaction.guild:  # 複製全域指令，同步到當前伺服器，不需等待 copy global command, sync to current server, no waiting required
             self.bot.tree.copy_global_to(guild=interaction.guild)
             result = await self.bot.tree.sync(guild=interaction.guild)
-        else:  # 同步到全域，需等待一小時
+        else:  # 同步到全域，需等待一小時 sync to global, estimated 1 hour waiting time
             result = await self.bot.tree.sync()
 
-        msg = f'已同步以下指令到{"全部" if area == 1 else "當前"}伺服器：{"、".join(cmd.name for cmd in result)}'
+        msg = f'command syncronized to{"all" if area == 1 else "current"}server:{",".join(cmd.name for cmd in result)}'
         await interaction.edit_original_response(content=msg)
 
-    # 顯示機器人相關狀態
-    @app_commands.command(name="status", description="顯示小幫手狀態")
+    # 顯示機器人相關狀態 display bot's status
+    @app_commands.command(name="status", description="display helper's status")
     @app_commands.choices(
         option=[
-            Choice(name="延遲", value=0),
-            Choice(name="已連接伺服器數量", value=1),
-            Choice(name="已連接伺服器名稱", value=2),
+            Choice(name="ping", value=0),
+            Choice(name="number of connected servers", value=1),
+            Choice(name="server names", value=2),
         ]
     )
     @SlashCommandLogger
     async def slash_status(self, interaction: discord.Interaction, option: int):
         if option == 0:
-            await interaction.response.send_message(f"延遲：{round(self.bot.latency*1000)} 毫秒")
+            await interaction.response.send_message(f"ping: {round(self.bot.latency*1000)} ms")
         elif option == 1:
-            await interaction.response.send_message(f"已連接 {len(self.bot.guilds)} 個伺服器")
+            await interaction.response.send_message(f"connected {len(self.bot.guilds)} servers")
         elif option == 2:
             await interaction.response.defer()
             names = [guild.name for guild in self.bot.guilds]
             for i in range(0, len(self.bot.guilds), 100):
-                msg = "、".join(names[i : i + 100])
-                embed = discord.Embed(title=f"已連接伺服器名稱({i + 1})", description=msg)
+                msg = ",".join(names[i : i + 100])
+                embed = discord.Embed(title=f"server names({i + 1})", description=msg)
                 await interaction.followup.send(embed=embed)
 
-    # 使用系統命令
-    @app_commands.command(name="system", description="使用系統命令(操作cog、更改機器人狀態)")
-    @app_commands.rename(option="選項", param="參數")
+    # 使用系統命令 use system command
+    @app_commands.command(name="system", description="use system command(control cog, change bot status)")
+    @app_commands.rename(option="option", param="parameter")
     @app_commands.choices(
         option=[
             Choice(name="load", value="load"),
@@ -80,38 +80,38 @@ class Admin(commands.Cog):
             *,
             pass_self: bool = False,
         ):
-            if param is None:  # 操作全部cog
+            if param is None:  # control all cog
                 for filepath in Path("./cogs").glob("**/*.py"):
                     cog_name = Path(filepath).stem
                     if pass_self and cog_name == "admin":
                         continue
                     await func(f"cogs.{cog_name}")
-            else:  # 操作單一cog
+            else:  # sintrol one cog
                 await func(f"cogs.{param}")
 
         if option == "load":  # Load cogs
             await operateCogs(self.bot.load_extension, param, pass_self=True)
-            await interaction.response.send_message(f"{param or '全部'}指令集載入完成")
+            await interaction.response.send_message(f"{param or 'all'}command loaded succesfully")
 
         elif option == "unload":  # Unload cogs
             await operateCogs(self.bot.unload_extension, param, pass_self=True)
-            await interaction.response.send_message(f"{param or '全部'}指令集卸載完成")
+            await interaction.response.send_message(f"{param or 'all'}command unloaded succesfully")
 
         elif option == "reload":  # Reload cogs
             await operateCogs(self.bot.reload_extension, param)
-            await interaction.response.send_message(f"{param or '全部'}指令集重新載入完成")
+            await interaction.response.send_message(f"{param or 'all'}command reloaded succesfully")
 
         elif option == "presence" and param is not None:  # Change presence string
             self.presence_string = param.split(",")
-            await interaction.response.send_message(f"Presence list已變更為：{self.presence_string}")
+            await interaction.response.send_message(f"Presence listchanged to: {self.presence_string}")
 
-        elif option == "claimdailyreward":  # 立即執行領取每日獎勵
-            await interaction.response.send_message("開始執行每日自動簽到")
+        elif option == "claimdailyreward":  # start daily reward claim
+            await interaction.response.send_message("Autoamaticaly claim daily rewards")
             asyncio.create_task(automation.claim_daily_reward(self.bot))
 
-    # 設定config配置檔案的參數值
-    @app_commands.command(name="config", description="更改config配置內容")
-    @app_commands.rename(option="選項", value="值")
+    # daily config values
+    @app_commands.command(name="config", description="daily config values")
+    @app_commands.rename(option="option", value="values")
     @app_commands.choices(
         option=[
             Choice(name="schedule_daily_reward_time", value="schedule_daily_reward_time"),
@@ -133,10 +133,10 @@ class Admin(commands.Cog):
             setattr(config, option, int(value))
         elif option in ["schedule_loop_delay"]:
             setattr(config, option, float(value))
-        await interaction.response.send_message(f"已將{option}的值設為: {value}")
+        await interaction.response.send_message(f"has been{option}changed to: {value}")
 
-    @app_commands.command(name="maintenance", description="設定遊戲維護時間，輸入0表示將維護時間設定為關閉")
-    @app_commands.rename(month="月", day="日", hour="點", duration="維護幾小時")
+    @app_commands.command(name="maintenance", description="setup maintenance schuedle")
+    @app_commands.rename(month="month", day="day", hour="hour", duration="duration")
     @SlashCommandLogger
     async def slash_maintenance(
         self,
@@ -148,7 +148,7 @@ class Admin(commands.Cog):
     ):
         if month == 0 or day == 0:
             config.game_maintenance_time = None
-            await interaction.response.send_message("已將維護時間設定為：關閉")
+            await interaction.response.send_message("maintenance schedule has been set to: off")
         else:
             now = datetime.now()
             start_time = datetime(
@@ -156,9 +156,9 @@ class Admin(commands.Cog):
             )
             end_time = start_time + timedelta(hours=duration)
             config.game_maintenance_time = (start_time, end_time)
-            await interaction.response.send_message(f"已將維護時間設定為：{start_time} ~ {end_time}")
+            await interaction.response.send_message(f"maintenance schedule has been set to:{start_time} ~ {end_time}")
 
-    # 每一定時間更改機器人狀態
+    # 每一定時間更改機器人狀態 change bot's status
     @tasks.loop(minutes=1)
     async def change_presence(self):
         length = len(self.presence_string)
@@ -166,7 +166,7 @@ class Admin(commands.Cog):
         if n < length:
             await self.bot.change_presence(activity=discord.Game(self.presence_string[n]))
         elif n == length:
-            await self.bot.change_presence(activity=discord.Game(f"{len(self.bot.guilds)} 個伺服器"))
+            await self.bot.change_presence(activity=discord.Game(f"{len(self.bot.guilds)} servers"))
 
     @change_presence.before_loop
     async def before_change_presence(self):
