365a90bc5e91be64c13909cb508de72e6688e01b
Aleytia <scarletdevil25@gmail.com>
2023-02-17 15:02:28 -0500

    Translate genshin genshin_info to English

--- cogs/genshin_info.py
+++ cogs/genshin_info.py
@@ -15,7 +15,7 @@ from yuanshen import draw, genshin_app, parser
 
 
 class RealtimeNotes:
-    """即時便箋"""
+    #即時便箋 real time notes
 
     @staticmethod
     async def notes(
@@ -36,7 +36,7 @@ class RealtimeNotes:
 
 
 class TravelerDiary:
-    """旅行者札記"""
+    #旅行者札記 Traveler's Diary
 
     @staticmethod
     async def diary(
@@ -56,7 +56,7 @@ class TravelerDiary:
 
 
 class RecordCard:
-    """遊戲紀錄卡片"""
+    #遊戲紀錄卡片 Game Record Card
 
     @staticmethod
     async def card(
@@ -91,10 +91,10 @@ class RecordCard:
 
 
 class Characters:
-    """角色一覽"""
+    #角色一覽 Characters
 
     class Dropdown(discord.ui.Select):
-        """選擇角色的下拉選單"""
+        #選擇角色的下拉選單 Chratacter options
 
         def __init__(
             self,
@@ -115,7 +115,7 @@ class Characters:
                 for i, c in enumerate(characters)
             ]
             super().__init__(
-                placeholder=f"選擇角色 (第 {index}~{index + len(characters) - 1} 名)",
+                placeholder=f"select character: (number{index}~{index + len(characters) - 1})",
                 min_values=1,
                 max_values=1,
                 options=options,
@@ -126,13 +126,13 @@ class Characters:
         async def callback(self, interaction: discord.Interaction):
             embed = parser.parse_character(self.characters[int(self.values[0])])
             embed.set_author(
-                name=f"{self.user.display_name} 的角色一覽",
+                name=f"{self.user.display_name}'s character",
                 icon_url=self.user.display_avatar.url,
             )
             await interaction.response.edit_message(content=None, embed=embed)
 
     class DropdownView(discord.ui.View):
-        """顯示角色下拉選單的View，依照選單欄位上限25個分割選單"""
+        #顯示角色下拉選單的View，依照選單欄位上限25個分割選單 display character dropdown view, max menu display slots are 25
 
         def __init__(
             self,
@@ -156,14 +156,14 @@ class Characters:
             await interaction.edit_original_response(embed=EmbedTemplate.error(e))
         else:
             view = Characters.DropdownView(user, characters)
-            await interaction.edit_original_response(content="請選擇角色：", view=view)
+            await interaction.edit_original_response(content="Choose a character:", view=view)
 
 
 class Notices:
-    """原神遊戲內的遊戲與活動公告"""
+    #原神遊戲內的遊戲與活動公告 Genshin game and event notices
 
     class Dropdown(discord.ui.Select):
-        """選擇公告的下拉選單"""
+        #選擇公告的下拉選單 Notice selection menu
 
         def __init__(self, notices: Sequence[genshin.models.Announcement], placeholder: str):
             self.notices = notices
@@ -187,13 +187,13 @@ class Notices:
             super().__init__(timeout=config.discord_view_long_timeout)
 
         async def interaction_check(self, interaction: discord.Interaction) -> bool:
-            # 避免短時間內太多人按導致聊天版面混亂
+            #避免短時間內太多人按導致聊天版面混亂 to prevent the chat crashing due to too much people interacting simultaneousl
             if (
                 self.last_response_time is not None
                 and (interaction.created_at - self.last_response_time).seconds < 3
             ):
                 await interaction.response.send_message(
-                    embed=EmbedTemplate.normal("短時間內(太多人)點選，請稍後幾秒再試..."), ephemeral=True
+                    embed=EmbedTemplate.normal("too many user interactions, please try again later...."), ephemeral=True
                 )
                 return False
             else:
@@ -209,13 +209,13 @@ class Notices:
         except Exception as e:
             await interaction.edit_original_response(embed=EmbedTemplate.error(e))
         else:
-            # 將公告分成活動公告、遊戲公告、祈願公告三類
+            # 將公告分成活動公告、遊戲公告、祈願公告三類 sort the game's notifications into 3 type: event notice, game notice, and wish notice
             game: list[genshin.models.Announcement] = []
             event: list[genshin.models.Announcement] = []
             wish: list[genshin.models.Announcement] = []
             for notice in notices:
                 if notice.type == 1:
-                    if "祈願" in notice.subtitle:
+                    if "Wish" in notice.subtitle:
                         wish.append(notice)
                     else:
                         event.append(notice)
@@ -224,26 +224,26 @@ class Notices:
 
             view = Notices.View()
             if len(game) > 0:
-                view.add_item(Notices.Dropdown(game, "遊戲公告："))
+                view.add_item(Notices.Dropdown(game, "Game Announcement:"))
             if len(event) > 0:
-                view.add_item(Notices.Dropdown(event, "活動公告："))
+                view.add_item(Notices.Dropdown(event, "Event Announcement"))
             if len(wish) > 0:
-                view.add_item(Notices.Dropdown(wish, "祈願卡池："))
+                view.add_item(Notices.Dropdown(wish, "Banner Announcement:"))
             await interaction.edit_original_response(view=view)
 
 
-class GenshinInfo(commands.Cog, name="原神資訊"):
-    """斜線指令"""
+class GenshinInfo(commands.Cog, name="Genshin info"):
+    #"""斜線指令""" slash commands
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     # -------------------------------------------------------------
-    # 取得使用者即時便箋資訊(樹脂、洞天寶錢、派遣...等)
-    @app_commands.command(name="notes即時便箋", description="查詢即時便箋，包含樹脂、洞天寶錢、探索派遣...等")
-    @app_commands.rename(shortForm="顯示格式", user="使用者")
-    @app_commands.describe(shortForm="選擇顯示完整或簡約格式(省略每日、週本、探索派遣)", user="查詢其他成員的資料，不填寫則查詢自己")
-    @app_commands.choices(shortForm=[Choice(name="完整", value=0), Choice(name="簡約", value=1)])
+    # 取得使用者即時便箋資訊(樹脂、洞天寶錢、派遣...等) "obtain user's real-time traveler's diary info (resin, realm currency, expedition etc.
+    @app_commands.command(name="Real Time Notes", description="check info about real-time notes, including resin, realm currency, expeditions etc.")
+    @app_commands.rename(shortForm="display format", user="user")
+    @app_commands.describe(shortForm="select full display or simplified display (will not display dailies, weeklies, expeditions))", user="check informtion of another user, will check current user's infor if no response is received.")
+    @app_commands.choices(shortForm=[Choice(name="complete", value=0), Choice(name="simplefied", value=1)])
     @SlashCommandLogger
     async def slash_notes(
         self,
@@ -254,15 +254,15 @@ class GenshinInfo(commands.Cog, name="原神資訊"):
         await RealtimeNotes.notes(interaction, user or interaction.user, shortForm=bool(shortForm))
 
     # -------------------------------------------------------------
-    # 取得使用者旅行者札記
-    @app_commands.command(name="diary旅行者札記", description="查詢旅行者札記(原石、摩拉收入)")
-    @app_commands.rename(month="月份")
-    @app_commands.describe(month="請選擇要查詢的月份")
+    # 取得使用者旅行者札記 obtain user's traveler's diary
+    @app_commands.command(name="Traveler's Diary", description="check traveler's diary's info (primogems, mora)")
+    @app_commands.rename(month="Month")
+    @app_commands.describe(month="Choose a month")
     @app_commands.choices(
         month=[
-            Choice(name="這個月", value=0),
-            Choice(name="上個月", value=-1),
-            Choice(name="上上個月", value=-2),
+            Choice(name="Current Month", value=0),
+            Choice(name="Previous Month", value=-1),
+            Choice(name="Two Months ago", value=-2),
         ]
     )
     @SlashCommandLogger
@@ -272,14 +272,14 @@ class GenshinInfo(commands.Cog, name="原神資訊"):
         await TravelerDiary.diary(interaction, interaction.user, month)
 
     # -------------------------------------------------------------
-    # 產生遊戲紀錄卡片
-    @app_commands.command(name="card紀錄卡片", description="產生原神個人遊戲紀錄卡片")
-    @app_commands.rename(option="選項", user="使用者")
-    @app_commands.describe(option="選擇要查詢數據總覽或是世界探索度", user="查詢其他成員的資料，不填寫則查詢自己")
+    # 產生遊戲紀錄卡片 generate game record card
+    @app_commands.command(name="Record Card", description="generate personal genshin impact record card")
+    @app_commands.rename(option="option", user="user")
+    @app_commands.describe(option="check record data or exploration progress", user="check informtion of another user, will check current user's infor if no response is received.")
     @app_commands.choices(
         option=[
-            Choice(name="數據總覽", value="RECORD"),
-            Choice(name="世界探索", value="EXPLORATION"),
+            Choice(name="Record Data", value="RECORD"),
+            Choice(name="Exploration Progress", value="EXPLORATION"),
         ]
     )
     @app_commands.checks.cooldown(1, config.slash_cmd_cooldown)
@@ -298,20 +298,20 @@ class GenshinInfo(commands.Cog, name="原神資訊"):
     ):
         if isinstance(error, app_commands.CommandOnCooldown):
             await interaction.response.send_message(
-                embed=EmbedTemplate.error(f"產生卡片的間隔為{config.slash_cmd_cooldown}秒，請稍後再使用~"),
+                embed=EmbedTemplate.error(f"Card Generation on cooldwon{config.slash_cmd_cooldown}seconds, try again later"),
                 ephemeral=True,
             )
 
     # -------------------------------------------------------------
-    # 個人所有角色一覽
-    @app_commands.command(name="character角色一覽", description="公開展示我的所有角色")
+    # 個人所有角色一覽 Character Showcase
+    @app_commands.command(name="characters", description="Publicky Display my characters")
     @SlashCommandLogger
     async def slash_characters(self, interaction: discord.Interaction):
         await Characters.characters(interaction, interaction.user)
 
     # -------------------------------------------------------------
-    # 遊戲公告與活動公告
-    @app_commands.command(name="notices原神公告", description="顯示原神的遊戲公告與活動公告")
+    # 遊戲公告與活動公告 game announcements and event announcements
+    @app_commands.command(name="Notices", description="display genshin impact's notices and event notices")
     @SlashCommandLogger
     async def slash_notices(self, interaction: discord.Interaction):
         await Notices.notices(interaction)
@@ -321,13 +321,13 @@ async def setup(client: commands.Bot):
     await client.add_cog(GenshinInfo(client))
 
     # -------------------------------------------------------------
-    # 下面為Context Menu指令
-    @client.tree.context_menu(name="即時便箋")
+    # 下面為Context Menu指令 context menu displayed at bottom
+    @client.tree.context_menu(name="Real Time Notes")
     @ContextCommandLogger
     async def context_notes(interaction: discord.Interaction, user: discord.User):
         await RealtimeNotes.notes(interaction, user)
 
-    @client.tree.context_menu(name="遊戲紀錄卡片")
+    @client.tree.context_menu(name="Record Card")
     @ContextCommandLogger
     async def context_card(interaction: discord.Interaction, user: discord.User):
         await RecordCard.card(interaction, user, "RECORD")
